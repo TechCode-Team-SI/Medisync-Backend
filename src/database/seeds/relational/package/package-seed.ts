63cf9757298fce67f02db6e1ff9ac8448e5f9be6
zetiuz <arnoldus.rf@gmail.com>
2024-08-31 21:55:25 -0400

    fix: add all package-seed

--- src/database/seeds/relational/package/package-seed.ts
+++ src/database/seeds/relational/package/package-seed.ts
@@ -8,6 +8,46 @@ const packages: Packages = [
     name: 'Cardiologia',
     slug: 'cardiologia',
   },
+  {
+    name: 'Medicina general',
+    slug: 'medicina-general',
+  },
+  {
+    name: 'Cirugía',
+    slug: 'cirugia',
+  },
+  {
+    name: 'Gineco-Obstetricia',
+    slug: 'gineco-obstetricia',
+  },
+  {
+    name: 'Pediatría',
+    slug: 'pediatria',
+  },
+  {
+    name: 'Laboratorio',
+    slug: 'laboratorio',
+  },
+  {
+    name: 'Radiología',
+    slug: 'radiologia',
+  },
+  {
+    name: 'Farmacia',
+    slug: 'farmacia',
+  },
+  {
+    name: 'Anestesia',
+    slug: 'anestesia',
+  },
+  {
+    name: 'Hemoterapia',
+    slug: 'hemoterapia',
+  },
+  {
+    name: 'Emergencia',
+    slug: 'emergencia',
+  },
 ];
 
 export default packages;
