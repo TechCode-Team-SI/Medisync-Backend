d4230118620223fda14a02b51e1c1f511a4cce7b
zetiuz <arnoldus.rf@gmail.com>
2024-10-21 21:55:03 -0400

    fix: add permissions to field-questions

--- src/field-questions/field-questions.module.ts
+++ src/field-questions/field-questions.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { FieldQuestionsService } from './field-questions.service';
 import { FieldQuestionsController } from './field-questions.controller';
 import { RelationalFieldQuestionPersistenceModule } from './infrastructure/persistence/relational/relational-persistence.module';
+import { permissionsModule } from 'src/permissions/permissions.module';
 
 @Module({
-  imports: [RelationalFieldQuestionPersistenceModule],
+  imports: [RelationalFieldQuestionPersistenceModule, permissionsModule],
   controllers: [FieldQuestionsController],
   providers: [FieldQuestionsService],
   exports: [FieldQuestionsService, RelationalFieldQuestionPersistenceModule],
