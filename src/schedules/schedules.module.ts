048d002858f6f5e9d99c4fce959916bf77c541c7
zetiuz <arnoldus.rf@gmail.com>
2024-10-22 19:52:38 -0400

    fix: add permissions to schedules

--- src/schedules/schedules.module.ts
+++ src/schedules/schedules.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SchedulesService } from './schedules.service';
 import { SchedulesController } from './schedules.controller';
 import { RelationalSchedulePersistenceModule } from './infrastructure/persistence/relational/relational-persistence.module';
+import { permissionsModule } from 'src/permissions/permissions.module';
 
 @Module({
-  imports: [RelationalSchedulePersistenceModule],
+  imports: [RelationalSchedulePersistenceModule, permissionsModule],
   controllers: [SchedulesController],
   providers: [SchedulesService],
   exports: [SchedulesService, RelationalSchedulePersistenceModule],
