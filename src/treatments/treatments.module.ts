7e63101aff3835fff341bf38dd853639b6a36cfd
zetiuz <arnoldus.rf@gmail.com>
2024-10-22 20:47:17 -0400

    fix: add permissions to treatments

--- src/treatments/treatments.module.ts
+++ src/treatments/treatments.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TreatmentsService } from './treatments.service';
 import { treatmentsController } from './treatments.controller';
 import { RelationaltreatmentPersistenceModule } from './infrastructure/persistence/relational/relational-persistence.module';
+import { permissionsModule } from 'src/permissions/permissions.module';
 
 @Module({
-  imports: [RelationaltreatmentPersistenceModule],
+  imports: [RelationaltreatmentPersistenceModule, permissionsModule],
   controllers: [treatmentsController],
   providers: [TreatmentsService],
   exports: [TreatmentsService, RelationaltreatmentPersistenceModule],
