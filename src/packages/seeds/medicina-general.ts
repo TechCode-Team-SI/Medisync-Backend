b901c225939788a3f3ba2d5f9f1d1d33418f1152
REDT <rafaeledazatorres@gmail.com>
2024-10-22 22:33:35 -0400

    add registers of  illnesses, injuries, treatments, pathologies, symptoms in gineco-obstetricia, hemoterapia, medicina general, pediatria

--- src/packages/seeds/medicina-general.ts
+++ src/packages/seeds/medicina-general.ts
@@ -21,11 +21,210 @@ const installationModule: InstallationModule = {
       },
     ],
   },
-  illnesses: [],
-  injuries: [],
-  treatments: [],
-  pathologies: [],
-  symptoms: [],
+  illnesses: [
+    {
+      id: 'ff05bba2-a208-4593-8ba0-f4c123dd1e77',
+      name: 'Gripe',
+      description:
+        'Enfermedad viral aguda que afecta principalmente al sistema respiratorio.',
+    },
+    {
+      id: '1fa22fda-a517-4062-8f90-719fc505bf97',
+      name: 'Diabetes',
+      description:
+        'Enfermedad crónica que se caracteriza por niveles altos de azúcar en la sangre.',
+    },
+    {
+      id: '6fd96b36-b3ea-4301-a088-026f1b9813b7',
+      name: 'Hipertensión',
+      description:
+        'Enfermedad crónica caracterizada por una presión arterial elevada.',
+    },
+    {
+      id: 'eed3ae1e-ba09-4ac5-b292-9d9d902edfb5',
+      name: 'Asma',
+      description:
+        'Enfermedad crónica que provoca inflamación de las vías respiratorias.',
+    },
+    {
+      id: 'e2317b68-75df-4b6d-b5ea-8259b281b86b',
+      name: 'Artritis',
+      description: 'Inflamación de una o más articulaciones.',
+    },
+    {
+      id: 'fa462680-091a-4bad-9449-269ed1902aa8',
+      name: 'Migraña',
+      description:
+        'Trastorno neurológico caracterizado por dolores de cabeza intensos y recurrentes.',
+    },
+    {
+      id: '6162ea43-a122-4b4d-8752-b600761f2a44',
+      name: 'Depresión',
+      description:
+        'Trastorno del estado de ánimo que causa sentimientos de tristeza, pérdida de interés y baja energía.',
+    },
+    {
+      id: '78017ab3-f5e1-41e1-8d21-2516a6fe3ed9',
+      name: 'Ansiedad',
+      description:
+        'Trastorno mental caracterizado por sentimientos de preocupación excesiva e incontrolable.',
+    },
+    {
+      id: '1e7a4313-43d4-4410-a4e5-8f8d3d9548d1',
+      name: 'Insomnio',
+      description: 'Dificultad para conciliar el sueño o mantenerlo.',
+    },
+    {
+      id: 'dc160717-7e5e-4172-b231-3d6879c7a4e1',
+      name: 'Enfermedad de Alzheimer',
+      description:
+        'Enfermedad neurodegenerativa que afecta la memoria y otras funciones cognitivas.',
+    },
+  ],
+  injuries: [
+    {
+      id: 'da562640-60d5-4876-8d52-66f99bcc2b85',
+      name: 'Esguince de tobillo',
+      description: 'Lesión de los ligamentos del tobillo.',
+    },
+    {
+      id: '3523e1ab-c72d-4aec-9245-f1fb58795403',
+      name: 'Fractura de muñeca',
+      description: 'Rotura de uno o más huesos de la muñeca.',
+    },
+    {
+      id: '7daf5338-7320-4609-be80-42f1e5592901',
+      name: 'Luxación de hombro',
+      description:
+        'Desplazamiento de la cabeza del húmero de la cavidad glenoidea.',
+    },
+    {
+      id: '4e41d114-683b-48b3-8143-78a68994c825',
+      name: 'Conmoción cerebral',
+      description: 'Lesión cerebral traumática leve.',
+    },
+    {
+      id: 'cd920b5b-e4c0-44aa-8147-5ddc6c2d7339',
+      name: 'Quemadura',
+      description:
+        'Lesión de la piel causada por el calor, el frío, sustancias químicas o radiación.',
+    },
+    {
+      id: 'e662f807-ccc4-4422-906d-52acb18ab61e',
+      name: 'Corte',
+      description: 'Herida producida por un objeto cortante.',
+    },
+    {
+      id: '4e46bd5f-679f-443c-89df-9078f37b7e7e',
+      name: 'Esguince de rodilla',
+      description: 'Lesión de los ligamentos de la rodilla.',
+    },
+  ],
+  treatments: [
+    {
+      id: '6eb2adb5-f591-4bf4-9e55-2fb070b0ee58',
+      name: 'Antibióticos',
+      description:
+        'Medicamentos utilizados para tratar infecciones bacterianas.',
+    },
+    {
+      id: '7f85f662-c9cb-4e92-bc47-e3b61109a5fe',
+      name: 'Fisioterapia',
+      description:
+        'Tratamiento que utiliza ejercicios y técnicas manuales para restaurar la función y movilidad.',
+    },
+    {
+      id: '7fdaa154-5653-4753-9926-ea80a5bdd8fc',
+      name: 'Medicamentos para el dolor',
+      description: 'Fármacos utilizados para aliviar el dolor.',
+    },
+    {
+      id: 'e377c857-ad12-4583-b967-a2f2cab5f507',
+      name: 'Cirugía',
+      description:
+        'Procedimiento médico que implica la incisión del cuerpo para tratar una enfermedad o lesión.',
+    },
+    {
+      id: '6f1e5c6f-b4b0-4df5-bf35-d77681bf421f',
+      name: 'Radioterapia',
+      description:
+        'Tratamiento que utiliza radiación para destruir células cancerosas.',
+    },
+    {
+      id: 'a865c725-26bc-455d-9bf5-cca12ec6cad9',
+      name: 'Quimioterapia',
+      description:
+        'Tratamiento que utiliza medicamentos para destruir células cancerosas.',
+    },
+  ],
+  pathologies: [
+    {
+      id: 'c218b54d-72a5-45f7-8175-df9b0024ddaf',
+      name: 'Arteriosclerosis',
+      description: 'Endurecimiento y engrosamiento de las arterias.',
+    },
+    {
+      id: '23d0bf05-7bdf-417b-a7da-0505434ef7fc',
+      name: 'Insuficiencia cardíaca',
+      description:
+        'Incapacidad del corazón para bombear suficiente sangre al cuerpo.',
+    },
+    {
+      id: '08e246d3-1fc8-4233-8c89-75410cb49e61',
+      name: 'Cáncer',
+      description:
+        'Enfermedad caracterizada por un crecimiento anormal de las células.',
+    },
+    {
+      id: '2e580449-620c-4910-a6ec-e8e259f0235b',
+      name: 'Enfermedad renal crónica',
+      description: 'Pérdida progresiva de la función renal.',
+    },
+    {
+      id: '68c9770b-b037-4a31-8392-cdd97f4d0985',
+      name: 'Enfermedad pulmonar obstructiva crónica (EPOC)',
+      description:
+        'Enfermedad pulmonar inflamatoria que causa dificultad para respirar.',
+    },
+  ],
+  symptoms: [
+    {
+      id: 'c62ad6f0-ce43-46e2-b9fa-cffbf43cc5a0',
+      name: 'Fiebre',
+      description:
+        'Aumento de la temperatura corporal por encima de lo normal.',
+    },
+    {
+      id: '4094c958-3c4a-470e-b09d-a34145821306',
+      name: 'Dolor de cabeza',
+      description: 'Sensación de dolor o molestia en la cabeza.',
+    },
+    {
+      id: '1bea4c22-847a-427f-ac39-3abb14211d2a',
+      name: 'Tos',
+      description: 'Expulsión violenta del aire de los pulmones.',
+    },
+    {
+      id: '',
+      name: 'Dificultad para respirar',
+      description: 'Sensación de falta de aire.',
+    },
+    {
+      id: 'c33e2e90-f480-4e5f-aad0-58f4fd9caed9',
+      name: 'Nauseas',
+      description: 'Sensación de malestar en el estómago con ganas de vomitar.',
+    },
+    {
+      id: 'c6f26cce-640e-4c1c-b930-4820a798aa02',
+      name: 'Vómitos',
+      description: 'Expulsión violenta del contenido del estómago por la boca.',
+    },
+    {
+      id: 'bec7fe62-c3d6-4991-a2b8-7cdd2819cdaf',
+      name: 'Diarrea',
+      description: 'Evacuaciones intestinales frecuentes y acuosas.',
+    },
+  ],
 };
 
 export default installationModule;
--- src/packages/seeds/pediatria.ts
+++ src/packages/seeds/pediatria.ts
@@ -33,11 +33,223 @@ const installationModule: InstallationModule = {
       },
     ],
   },
-  illnesses: [],
-  injuries: [],
-  treatments: [],
-  pathologies: [],
-  symptoms: [],
+  illnesses: [
+    {
+      id: 'b711e6b2-0a54-43a8-b494-b10228851c3d',
+      name: 'Resfriado común',
+      description: 'Infección viral de las vías respiratorias superiores.',
+    },
+    {
+      id: 'd2e0c934-421e-41c1-8e1b-93100970d496',
+      name: 'Gripe',
+      description:
+        'Enfermedad viral aguda que afecta principalmente al sistema respiratorio.',
+    },
+    {
+      id: '52f4ab52-0ff4-4418-afce-1315580e7682',
+      name: 'Otitis media',
+      description: 'Inflamación del oído medio.',
+    },
+    {
+      id: '44d49925-9a55-4c77-a45c-693de65b11c1',
+      name: 'Bronquiolitis',
+      description:
+        'Inflamación de los bronquiolos, las vías respiratorias pequeñas.',
+    },
+    {
+      id: 'f5d241bc-517c-4c81-be57-e8b4a077a5f4',
+      name: 'Neumonía',
+      description: 'Inflamación de los pulmones.',
+    },
+    {
+      id: 'fc3f399a-315f-4079-99ff-2146b1d6c94d',
+      name: 'Meningitis',
+      description:
+        'Inflamación de las meninges, las membranas que recubren el cerebro y la médula espinal.',
+    },
+    {
+      id: '8a336bc1-fcf3-48d5-8865-76e995664a7f',
+      name: 'Gastroenteritis',
+      description: 'Inflamación del estómago y los intestinos.',
+    },
+    {
+      id: 'a253936a-fc7e-409f-9c05-d805a5601087',
+      name: 'Conjuntivitis',
+      description:
+        'Inflamación de la conjuntiva, la membrana que recubre el ojo.',
+    },
+    {
+      id: '2f014036-5cd2-4eaf-bf44-84625ba4ef68',
+      name: 'Urticaria',
+      description: 'Erupción cutánea con ronchas elevadas y pruriginosas.',
+    },
+    {
+      id: '153314f1-2f0e-432d-bb53-7a1dab8c1e43',
+      name: 'Asma',
+      description:
+        'Enfermedad crónica que provoca inflamación de las vías respiratorias.',
+    },
+  ],
+  injuries: [
+    {
+      id: '9539be96-c218-480b-9861-48aa46d0cbef',
+      name: 'Caídas',
+      description:
+        'Lesiones comunes en niños, pueden causar contusiones, fracturas o lesiones en la cabeza.',
+    },
+    {
+      id: 'cd384949-5ee3-4f2a-a453-79e0c7be8d11',
+      name: 'Fracturas',
+      description: 'Rotura de un hueso.',
+    },
+    {
+      id: '9975c7fa-fc27-4fe2-bfbc-9e50d4030246',
+      name: 'Contusiones',
+      description: 'Moretones o hematomas.',
+    },
+    {
+      id: 'f7312f9e-06ca-4adb-a15c-6dd582fdfcef',
+      name: 'Luxaciones',
+      description: 'Desplazamiento de un hueso de su articulación.',
+    },
+    {
+      id: 'd3ff1968-acc6-45a3-9ee6-fd780667f91d',
+      name: 'Quemaduras',
+      description:
+        'Lesiones de la piel causadas por el calor, el frío, sustancias químicas o radiación.',
+    },
+    {
+      id: 'a2c19428-c22b-4802-a411-6b6ecab60f38',
+      name: 'Picaduras de insectos',
+      description:
+        'Reacciones alérgicas o infecciones causadas por picaduras de insectos.',
+    },
+    {
+      id: 'd3914f3d-ed7c-4ffa-b428-e331786a60fc',
+      name: 'Objetos extraños en la nariz o los oídos',
+      description: 'Accidentes comunes en niños pequeños.',
+    },
+  ],
+  treatments: [
+    {
+      id: 'b9d4cbe3-009b-4f72-87ea-d37f27df4c65',
+      name: 'Medicamentos para el dolor y la fiebre',
+      description: 'Acetaminofén o ibuprofeno.',
+    },
+    {
+      id: '6073439e-4c07-4957-9b95-3aae70d12bb5',
+      name: 'Antibióticos',
+      description:
+        'Medicamentos utilizados para tratar infecciones bacterianas.',
+    },
+    {
+      id: '7a41b5fc-dd2c-4269-aae9-ba6f476a076e',
+      name: 'Líquidos',
+      description: 'Importantes para prevenir la deshidratación.',
+    },
+    {
+      id: '1ac0c93d-de0a-41e4-b5ba-a02f273f3190',
+      name: 'Descanso',
+      description: 'Permite que el cuerpo se recupere.',
+    },
+    {
+      id: '0d18f7f2-416e-429e-9485-941ced887250',
+      name: 'Compresas frías o calientes',
+      description: 'Para aliviar el dolor y la inflamación.',
+    },
+    {
+      id: '87d0b87e-d4cd-4ecc-adf7-c4e51612a67f',
+      name: 'Inhaladores',
+      description: 'Para tratar el asma y otras enfermedades respiratorias.',
+    },
+    {
+      id: 'afa78ace-341f-4d0c-833d-4cd2bd4eaa7f',
+      name: 'Vacunas',
+      description: 'Prevención de enfermedades infecciosas.',
+    },
+  ],
+  pathologies: [
+    {
+      id: '5368b70b-6435-4dea-bf2f-a78f1690b3e3',
+      name: 'Defectos congénitos',
+      description: 'Anomalías presentes al nacer.',
+    },
+    {
+      id: '64aa9ea5-757c-4013-96eb-5e0163fb9556',
+      name: 'Trastornos del desarrollo',
+      description:
+        'Retraso o dificultad en el desarrollo físico, cognitivo, social o emocional.',
+    },
+    {
+      id: 'eaf1226b-9cf4-4e0b-9250-bfce66c31033',
+      name: 'Trastornos del comportamiento',
+      description:
+        'Dificultades para controlar el comportamiento o las emociones.',
+    },
+    {
+      id: '8fb34ffa-bc88-4c61-a8d8-163b7056fbde',
+      name: 'Trastornos del aprendizaje',
+      description: 'Dificultades para aprender o procesar la información.',
+    },
+    {
+      id: 'ec4dcb8a-6539-4671-85dc-543e2db90312',
+      name: 'Trastornos del sueño',
+      description: 'Problemas para dormir o mantenerse dormido.',
+    },
+  ],
+  symptoms: [
+    {
+      id: 'd54de247-ed6f-4bfe-85e2-d29297a4b57f',
+      name: 'Fiebre',
+      description:
+        'Aumento de la temperatura corporal por encima de lo normal.',
+    },
+    {
+      id: '3cae0c57-4299-47a4-8529-cc180f01d9d1',
+      name: 'Tos',
+      description: 'Expulsión violenta del aire de los pulmones.',
+    },
+    {
+      id: '74ecc429-9dfc-4142-ac17-7ee4e4032627',
+      name: 'Dificultad para respirar',
+      description: 'Sensación de falta de aire.',
+    },
+    {
+      id: '073abb1f-4351-47cf-a3e4-cf080f6b1916',
+      name: 'Dolor de cabeza',
+      description: 'Sensación de dolor o molestia en la cabeza.',
+    },
+    {
+      id: 'deff64eb-5825-43e6-95d5-995c8f3c4c35',
+      name: 'Dolor de garganta',
+      description: 'Dolor o irritación en la garganta.',
+    },
+    {
+      id: '9bc8deda-002a-4784-b8d0-003156fc06a1',
+      name: 'Dolor abdominal',
+      description: 'Dolor en el estómago o el abdomen.',
+    },
+    {
+      id: 'c1f63e6a-4415-4507-8b08-49cb505f5461',
+      name: 'Vómitos',
+      description: 'Expulsión violenta del contenido del estómago por la boca.',
+    },
+    {
+      id: '3bb5bc69-7775-4f17-9340-4ad68c3632c0',
+      name: 'Diarrea',
+      description: 'Evacuaciones intestinales frecuentes y acuosas.',
+    },
+    {
+      id: '8dc47515-4801-4432-8eff-af92838cc496',
+      name: 'Rash',
+      description: 'Erupción cutánea.',
+    },
+    {
+      id: '6207b521-8400-439b-a20d-c9213063d4ab',
+      name: 'Irritabilidad',
+      description: 'Estado de mal humor o fácilmente irritado.',
+    },
+  ],
 };
 
 export default installationModule;
