997a7d1cd0691ecf882e03df6d5d8634887a6179
zetiuz <arnoldus.rf@gmail.com>
2024-10-22 20:30:46 -0400

    fix: add permissions to symptoms

--- src/symptoms/symptoms.module.ts
+++ src/symptoms/symptoms.module.ts
@@ -3,9 +3,10 @@ import { SymptomsService } from './symptoms.service';
 import { SymptomsController } from './symptoms.controller';
 import { RelationalSymptomPersistenceModule } from './infrastructure/persistence/relational/relational-persistence.module';
 import { UsersModule } from 'src/users/users.module';
+import { permissionsModule } from 'src/permissions/permissions.module';
 
 @Module({
-  imports: [RelationalSymptomPersistenceModule, UsersModule],
+  imports: [RelationalSymptomPersistenceModule, UsersModule, permissionsModule],
   controllers: [SymptomsController],
   providers: [SymptomsService],
   exports: [SymptomsService, RelationalSymptomPersistenceModule],
