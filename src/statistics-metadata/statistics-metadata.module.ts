6dd54980de37c52badb2eb00f92e4ee0f813a0a5
chengkev <chengkev2000@gmail.com>
2024-11-26 16:40:42 -0400

    fix: create notification for statistic metadata

--- src/statistics-metadata/statistics-metadata.module.ts
+++ src/statistics-metadata/statistics-metadata.module.ts
@@ -4,12 +4,15 @@ import { StatisticsMetadataController } from './statistics-metadata.controller';
 import { RelationalStatisticsMetadataPersistenceModule } from './infrastructure/persistence/relational/relational-persistence.module';
 import { FieldQuestionsModule } from 'src/field-questions/field-questions.module';
 import { permissionsModule } from 'src/permissions/permissions.module';
+import { BullModule } from '@nestjs/bullmq';
+import { QueueName } from 'src/utils/queue-enum';
 
 @Module({
   imports: [
     RelationalStatisticsMetadataPersistenceModule,
     FieldQuestionsModule,
     permissionsModule,
+    BullModule.registerQueue({ name: QueueName.NOTIFICATION }),
   ],
   controllers: [StatisticsMetadataController],
   providers: [StatisticsMetadataService],
